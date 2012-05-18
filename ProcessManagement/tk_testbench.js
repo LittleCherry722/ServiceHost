f7360251c615ddac494c369a962d3d40f73603eb
Jens Hartwig <jhartwig@mycompany.com>
2012-05-18 16:56:59 +0000

    Testdaten erweitert.
    git-svn-id: svn://localhost/SBPM/trunk@9 242ca866-ce47-4be4-be7e-1aabc15e41bd

--- ProcessManagement/tk_testbench.js
+++ ProcessManagement/tk_testbench.js
@@ -1,7 +1,7 @@
 // new Objects
 gv_graph.addSubject("Employee", "Employee");
 gv_graph.addSubject("Manager", "Manager");
-gv_graph.addSubject("HR", "Human\nResources");
+gv_graph.addSubject("HR", "HumanResources");
 var gt_behav = null;
 
 // employee
@@ -16,9 +16,9 @@ gt_behav.addNode("act3", "adapt\nvacation\nrequest");
 gt_behav.addNode("end", "end");
 
 gt_behav.addEdge("start", "s1", "");
-gt_behav.addEdge("s1", "r1", "vacation\nrequest", "manager");
-gt_behav.addEdge("r1", "act2", "denial", "manager");
-gt_behav.addEdge("r1", "act1", "approval", "manager");
+gt_behav.addEdge("s1", "r1", "vacation\nrequest", "Manager");
+gt_behav.addEdge("r1", "act2", "denial", "Manager");
+gt_behav.addEdge("r1", "act1", "approval", "Manager");
 gt_behav.addEdge("act1", "end", "");
 gt_behav.addEdge("act2", "act3", "");
 gt_behav.addEdge("act2", "end", "adaption\nnot possible");
@@ -35,11 +35,11 @@ gt_behav.addNode("s2", "S");
 gt_behav.addNode("s3", "S");
 gt_behav.addNode("end");
 
-gt_behav.addEdge("rcv1", "act1", "vacation\nrequest", "employee");
+gt_behav.addEdge("rcv1", "act1", "vacation\nrequest", "Employee");
 gt_behav.addEdge("act1", "s1", "denial");
 gt_behav.addEdge("act1", "s2", "approval")
-gt_behav.addEdge("s1", "rcv1", "denial", "employee");
-gt_behav.addEdge("s2", "s3", "approval", "employee");
+gt_behav.addEdge("s1", "rcv1", "denial", "Employee");
+gt_behav.addEdge("s2", "s3", "approval", "Employee");
 gt_behav.addEdge("s3", "end", "approved\nvacation\nrequest", "hr");
 
 
