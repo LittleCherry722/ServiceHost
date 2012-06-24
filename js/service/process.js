699b561d21d3a864bd36e66de05c910ab17d8793
Philip Beyer <pbeyer@mycompany.com>
2012-06-24 16:48:03 +0000

    SBPM-41 Laden eines Prozesses klappt noch etwas besser: SBPM.VM.processVM.showProcess("Urlaubsantrag") als Beispiel.
    git-svn-id: svn://localhost/SBPM/trunk/ProcessManagement@90 242ca866-ce47-4be4-be7e-1aabc15e41bd

--- js/service/process.js
+++ js/service/process.js
@@ -1,42 +1,89 @@
 SBPM.Service.Process = {
-	afterRender : function() {
+	newProcess : function(processName) {
+if(this.createProcess(processName) == 0) {
+	$("#freeow").freeow("Create process", "Could not create process \"" + processName +"\".", {
+		classes: [,"error"],
+		autohide: true
+	});
+} 
+else {
+	$("#freeow").freeow("Create process", "Process \"" + processName + "\" successfully created.", {
+		classes: [,"ok"],
+		autohide: true
+	});
 
-		$("#slctSbj").chosen();
-	},
 
-	showSubjectView : function() {
-		SBPM.VM.activeMainViewIndex(1);
-		SBPM.VM.activeProcessViewIndex(0);
 
-	},
-	showInternalView : function() {
-		SBPM.VM.activeMainViewIndex(1);
-		SBPM.VM.activeProcessViewIndex(1);
-	},
-	showChargeView : function() {
-		SBPM.VM.activeMainViewIndex(1);
-		SBPM.VM.activeProcessViewIndex(2);
-	},
 
-	displayProcess : function(processName) {
-		this.showSubjectView();
-		gv_graph.loadFromJSON(loadGraph(getProcessID(processName)));
-		gv_graph.init();
+		//showverantwortliche();
+		//setSubjectIDs();
+
+
+}
 	},
 
-	subjectAfterRender : function() {
-		gv_graph.init();
-		gf_paperChangeView("cv");
-					updateListOfSubjects();
-	},
-	internalAfterRender : function() {
-gf_clickedCVbehavior();
-			updateListOfSubjects();
-	},
-	chargeAfterRender : function() {
-showverantwortliche();
-			gv_graph.selectedNode = null;
-			updateListOfSubjects();
+	loadProcess : function(processName) {
+
+		gf_loadGraph(loadGraph(getProcessID(processName)));
 
 	},
+	saveAsProcess : function(processName) {
+
+	},
+	saveProcess : function() {
+
+	},
+	// create/remove process
+createProcess : function (processname){
+	return SBPM.DB.syncQuery("process.php", {"processname" : processname, "action" : "new"}, 0, defaultIDReturn);
+},
+deleteProcess : function (processname){
+	return SBPM.DB.syncQuery("process.php", {"processname" : processname, "action" : "remove"}, false, defaultRemoveReturn);
+},
+deleteProcessByID : function (processid){
+	return SBPM.DB.syncQuery("process.php", {"processid" : processid, "action" : "remove"}, false, defaultRemoveReturn);
+},
+// get processes
+getProcessName : function (processid){
+	return SBPM.DB.syncQuery("process.php", {"processid" : processid, "action" : "getname"}, "", function (json){
+		if (json["code"] == "ok")
+			return json["name"];});
+},
+getProcessID : function (processname){
+	return SBPM.DB.syncQuery("process.php", {"processname" : processname, "action" : "getid"}, 0, defaultIDReturn);
+},
+getAllProcesses : function (){
+	return SBPM.DB.syncQuery("process.php", {"action" : "getallprocesses"}, "", function (json){
+		if (json["code"] == "ok")
+			return json["processes"];});
+},
+getAllProcessesIDs : function (){
+	return SBPM.DB.syncQuery("process.php", {"action" : "getallprocessesids"}, "", function (json){
+		if (json["code"] == "ok")
+			return json["ids"];});
+},
+getAllStartableProcessIDs : function (userid){
+	return SBPM.DB.syncQuery("process.php", {"action" : "getallstartable", "userid" : userid}, "", function (json){
+		if (json["code"] == "ok")
+			return json["ids"];});
+},
+// load/save graph
+loadGraph : function (processid){
+	return SBPM.DB.syncQuery("process.php", {"processid" : processid, "action" : "load"}, "", function (json){
+		if (json["code"] == "ok"){
+			try {
+				return json["graph"];
+			}catch (e){
+				return "{}";
+			}
+		}else{
+			return "{}";
+		}
+	});
+},
+saveGraph : function (processid, graphAsJSON, startSubjectsAsJSON){
+	return SBPM.DB.syncQuery("process.php", {"processid" : processid, "action" : "save", "graph" : graphAsJSON, "subjects" : startSubjectsAsJSON}, false, function (json){
+		if (json["code"] == "ok")
+			return true;});	
+},
 }
