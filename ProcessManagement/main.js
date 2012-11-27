201716b9a683f95e1aa143ade78cf5f48525a327
Arne Link <mail@arne-link.de>
2012-11-27 20:39:01 +0100

    einige weitere änderungen, shim config hinzugefügt, weitere plugins eingebaut.

---
+++ ProcessManagement/main.js
@@ -0,0 +1,36 @@
+// Require.js allows us to configure shortcut alias
+require.config({
+	paths: {
+		"director":          "libs/director/director",
+		"jquery":            "libs/jquery/jquery",
+		"jquery.ui":         "libs/jquery/plugins/jquery-ui",
+		"jquery.freeow":     "libs/jquery/plugins/jquery.freeow",
+		"jquery.qtip":       "libs/jquery/plugins/jquery.qtip",
+		"jquery.scrollTo":   "libs/jquery/plugins/jquery.scrollTo",
+		"jquery.chosen":     "libs/jquery/plugins/jquery.chosen",
+		"jquery.bootstrap":  "libs/jquery/bootstrap.min",
+		"keymaster":         "libs/keymaster/keymaster",
+		"knockout":          "libs/knockout/knockout",
+		"knockout.mapping":  "libs/knockout/plugins/knockout.mapping",
+		"underscore":        "libs/underscore/underscore"
+	},
+	shim: {
+		"jquery.ui":         ["jquery"],
+		"jquery.freeow":     ["jquery"],
+		"jquery.chosen":     ["jquery"],
+		"jquery.qtip":       ["jquery"],
+		"jquery.scrollTo":   ["jquery"],
+		"jquery.bootstrap":  ["jquery"],
+		"knockout.mapping":  ["knockout"]
+	}
+
+});
+
+require([
+
+	// Load our app module and pass it to our definition function
+	'app',
+], function(App){
+	// The "app" dependency is passed in as "App"
+	App.initialize();
+});
