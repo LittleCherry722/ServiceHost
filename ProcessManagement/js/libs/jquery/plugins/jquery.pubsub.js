91a7b0f1b02a45386896504660c1154c609aa20f
Arne Link <mail@arne-link.de>
2012-12-19 10:24:46 +0100

    nicht mehr benötigte js libraries entfernt, pubsub auf den gleichen stand wie in der graphen bibliothek gebracht

--- ProcessManagement/js/libs/jquery/plugins/jquery.pubsub.js
+++ ProcessManagement/js/libs/jquery/plugins/jquery.pubsub.js
@@ -6,6 +6,10 @@
 
 	Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
 	http://dojofoundation.org/license for more information.
+	
+	
+	subscribers, subscribeOnce are additions by Arne Link
+	(c) 2012, S-BPM Groupware
 
 */	
 
@@ -29,6 +33,9 @@
 		//
 		//	|		$.publish("/some/topic", ["a","b","c"]);
 		cache[topic] && d.each(cache[topic], function(){
+			if ( !d.isArray(args) ) {
+                    args = [args]
+            }
 			this.apply(d, args || []);
 		});
 	};
@@ -72,15 +79,43 @@
 			}
 		});
 	};
-
+	
 	// List Subscribers
-	d.subscribers = function(/* String */topic) {
-		l = [];
-		cache[topic] && d.each(cache[topic], function(idx) {
-			l.push(this);
-		});
-		return l;
-	};
+    d.subscribers = function(/* String */topic) {
+            l = [];
+            cache[topic] && d.each(cache[topic], function(idx) {
+                    l.push(this);
+            });
+            return l;
+    };
+
+    d.subscribeOnce = function(/* String */topic, /* Function */callback){
+            // summary:
+            //              Register a callback on a named topic.
+            // topic: String
+            //              The channel to subscribe to
+            // callback: Function
+            //              The handler event. Anytime something is $.publish'ed on a 
+            //              subscribed channel, the callback will be called with the
+            //              published array as ordered arguments.
+            //
+            //      Only subscribes if callback is not already subscribed
+            //
+            // returns: Array
+            //              A handle which can be used to unsubscribe this particular subscription.
+            //      
+            // example:
+            //      |       $.subscribe("/some/topic", function(a, b, c){ /* handle data */ });
+            //
+            if(d.subscribers(topic).indexOf(callback) !== -1) {
+                    return [topic, callback]; // Array
+            }
+            if(!cache[topic]){
+                    cache[topic] = [];
+            }
+            cache[topic].push(callback);
+            return [topic, callback]; // Array
+    };
 
 })(jQuery);
 
