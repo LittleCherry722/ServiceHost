25f74bd25632098bf07258a2868ac73150d8a704
Philip Beyer <philipbeyer@onlinehome.de>
2013-01-19 17:22:07 +0100

    SBPM-314

--- ProcessManagement/js/viewmodels/process.js
+++ ProcessManagement/js/viewmodels/process.js
@@ -18,9 +18,6 @@ define([
 
 		this.currentProcess = currentProcess;
 
-pB = function(){
-	console.log(currentProcess());
-}
 
 		// The currently displayed graph
 		this.currentGraph = currentGraph;
@@ -82,6 +79,25 @@ pB = function(){
 		this.saveCurrentProcessAs = function() {
 			saveCurrentGraphAs( newProcessName() );
 		}
+		
+			//Import and export the graph.
+	
+		this.exportGraph = function() {
+			var graph = currentProcess().graph().graphString();
+			graph = graph.replace(/"role":"[^"]+/g, "\"role\":\"");
+			graph = graph.replace(/"routings":[^\]]+/g, "\"routings\":[");
+			console.log(graph);
+			this.graphText(graph);
+			
+		}
+	
+		this.graphText = ko.observable("");
+	
+		this.importGraph = function() {
+			currentProcess().graph().graphString(this.graphText());
+		}
+		
+		
 	}
 
 
@@ -322,6 +338,9 @@ pB = function(){
 		});
 
 	}
+
+
+
 
 	// Basic graph loading.
 	// Just load the graph from a JSON String and display it.
