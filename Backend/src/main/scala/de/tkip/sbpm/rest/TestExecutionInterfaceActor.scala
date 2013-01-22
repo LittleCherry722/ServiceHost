7edb707b334d684a6814d9da3a5a950f848ed8c4
david <d.dornseifer@online.de>
2013-01-22 10:44:26 +0100

    SBPM-284 Added TestExecutionInterfaceActor

---
+++ Backend/src/main/scala/de/tkip/sbpm/rest/TestExecutionInterfaceActor.scala
@@ -0,0 +1,126 @@
+package de.tkip.sbpm.rest
+
+import scala.concurrent.duration._
+import akka.actor.Actor
+import akka.event.Logging
+import akka.pattern.ask
+import akka.util.Timeout
+import de.tkip.sbpm.application._
+import spray.http.MediaTypes._
+import spray.routing._
+import scala.concurrent.Await
+import de.tkip.sbpm.model._
+import de.tkip.sbpm.rest.JsonProtocol._
+import spray.httpx.SprayJsonSupport._
+import spray.json._
+import scala.util.parsing.json.JSONArray
+import de.tkip.sbpm.application.miscellaneous._
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONArray
+
+/**
+ * This Actor is only used to process "get" REST calls regarding "execution" mixed with the debug trait
+ */
+// TODO when to choose HttpService and when HttpServiceActor
+class TestExecutionInterfaceActor extends Actor with HttpService {
+
+  val logger = Logging(context.system, this)
+
+  override def preStart() {
+    logger.debug(context.self + " starts.")
+  }
+
+  override def postStop() {
+    logger.debug(context.self + " stops.")
+  }
+
+  def actorRefFactory = context
+
+  def receive = runRoute({
+    formField("userid") { userId =>
+
+      get {
+        //READ
+        path(IntNumber) { processID =>
+          formField("subject") { (subject) =>
+            //return all information for a given process (graph, next actions (unique ID per available action), history)
+            implicit val timeout = Timeout(5 seconds)
+            
+            //val future1 = context.actorFor("/user/SubjectProviderManager") ? new ReadProcess(userId.toInt, processID.toInt)
+            val future2 = context.actorFor("/user/SubjectProviderManager") ? new GetHistory(userID = 1, processID = 1) with Debug
+            //val future3 = context.actorFor("/user/SubjectProviderManager") ? new GetAvailableActions(userId.toInt, processID.toInt)
+            val history = Await.result(future2, timeout.duration).asInstanceOf[HistoryAnswer];
+            
+            
+            /**
+            val result = for {
+            	graph <- future1.mapTo[Process] 
+            	history <- future2.mapTo[History] 
+            	actions <- future3.mapTo[Action] 
+            } yield List(graph, history, actions)
+            
+            
+            result onSuccess {
+              case jl: List[Object] => 
+                complete(new Envelope(Some(JsObject("instanceId" -> JsNumber(instanceId))), "ok"))
+                
+            }
+            
+            result onFailure {
+              case _ => complete("an error occured")
+            }
+            
+            */
+    
+            complete(history.toJson)
+          }
+        } ~
+          //LIST
+          path("") {
+            //List all executed process (for a given user)
+            implicit val timeout = Timeout(5 seconds)
+            val future = context.actorFor("/user/SubjectProviderManager") ? new ExecuteRequestAll(userId.toInt)
+            val list = Await.result(future, timeout.duration).asInstanceOf[ExecutedListAnswer]
+            complete(list.toJson)
+          }
+
+      } ~
+        delete {
+          //DELETE
+          path(IntNumber) { processID =>
+            //stop and delete given process
+            context.actorFor("/user/SubjectProviderManager") ! new KillProcess(userId.toInt)
+            complete("Process deleted")
+          }
+        } ~
+        put {
+          //CREATE
+          path("") {
+            formField("processId") { (processId) =>
+              implicit val timeout = Timeout(5 seconds)
+              val future = context.actorFor("/user/SubjectProviderManager") ? new ExecuteRequest(userId.toInt, processId.toInt)
+              val instanceId: Int = Await.result(future, timeout.duration).asInstanceOf[ProcessInstanceCreated].processInstanceID
+              complete(
+                //marshalling
+                new Envelope(Some(JsObject("instanceId" -> JsNumber(instanceId))), "ok"))
+            }
+          } ~
+            //UPDATE
+            path(IntNumber) { processID =>
+              formField("actionID") { (actionID) =>
+                //execute next step (chosen by actionID)
+
+                context.actorFor("/user/SubjectProviderManager") ! new RequestAnswer(processID.toInt, actionID)
+                complete("Process updated")
+              }
+            }
+        }
+
+    }
+  })
+
+}
+
