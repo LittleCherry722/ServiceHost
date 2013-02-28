f21c42b7596950d9ddbfd56601de135c6fbf09b8
Richard Stein <richard.stein@stud.tu-darmstadt.de>
2013-02-28 01:52:40 +0100

    SBPM-363 Beim parsen eines ProcessGraphen wird nun in den Transitionen die Kurzform des MessageTypes durch die lange Form ersetzt

--- Backend/src/main/scala/de/tkip/sbpm/application/miscellaneous/ProcessMarshalling.scala
+++ Backend/src/main/scala/de/tkip/sbpm/application/miscellaneous/ProcessMarshalling.scala
@@ -1,6 +1,7 @@
 package de.tkip.sbpm.application.miscellaneous
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ Map => MutableMap }
 import spray.json._
 import de.tkip.sbpm.application.miscellaneous.ProcessAttributes._
 import de.tkip.sbpm.model._
@@ -26,13 +27,12 @@ object parseSubjects {
 }
 
 /**
- * This object is responsible to create a ProcessGraph
+ * This objectfunction is responsible to create a ProcessGraph
  * out of the JSON representation
  */
 object parseGraph {
   // The marshalling case classes
-  // TODO messageType umwandeln zB m0 -> Receive Acbdas
-  private case class JGraph(process: Array[JSubject])
+  private case class JGraph(process: Array[JSubject], messages: JsValue)
   private case class JSubject(id: SubjectID, name: SubjectName, inputPool: Int, macros: Array[JBehavior])
   private case class JBehavior(nodes: Array[JNode], edges: Array[JEdge])
   private case class JNode(id: StateID, text: String, start: Boolean, end: Boolean, myType: String, options: JNodeOption)
@@ -48,42 +48,36 @@ object parseGraph {
     implicit val nodeFormat = jsonFormat6(JNode)
     implicit val behaviorFormat = jsonFormat2(JBehavior)
     implicit val subjectFormat = jsonFormat4(JSubject)
-    implicit val graphFormat = jsonFormat1(JGraph)
+    implicit val graphFormat = jsonFormat2(JGraph)
   }
   import JsonFormats._
 
-  /**
-   * This class holds a state while it is in creation, so it is possible
-   * to add transitions
-   */
-  private class StateCreator(val id: StateID,
-                             val name: SubjectName,
-                             val stateType: StateType) {
-    val transitions = new ArrayBuffer[Transition]
-
-    def addTransition(transition: Transition) {
-      transitions += transition
-    }
-
-    def createState: State = State(id, name, stateType, transitions.toArray)
-  }
+  // This map matches the shortversions and real versions of the message types
+  private var messageMap: Map[String, String] = null
 
   def apply(graph: String): ProcessGraph = {
     // TODO fehlerbehandlung bei falschem String
     // TODO type ersetzung ist so nicht effizient
-    ProcessGraph(
-      graph.replace("\"type\":", "\"myType\":").asJson.convertTo[JGraph]
-        .process.map(parseSubject(_)).toArray)
+
+    // parse the graph message count and the message object from the graph
+    val jgraph = graph.replace("\"type\":", "\"myType\":").asJson.convertTo[JGraph]
+    val jmessages = jgraph.messages.asJsObject()
+
+    // parse the message map from the json graph
+    messageMap = for ((k, v) <- jmessages.fields) yield (k, v.convertTo[String])
+
+    // create the processGraph by parsing all subjects
+    ProcessGraph(jgraph.process.map(parseSubject(_)).toArray)
   }
 
   private object parseSubject {
-    import scala.collection.mutable.{ Map => MutableMap }
     // TODO irgentwie elegant loesen
-    private var states = MutableMap[StateID, StateCreator]()
+    private var states: MutableMap[StateID, StateCreator] = null
     // TODO unique id's
     private var startID: StateID = -1
 
     def apply(subject: JSubject): Subject = {
+      // reset the statesmap
       states = MutableMap[StateID, StateCreator]()
       // First create an unique start and end state
       states(startID) = new StateCreator(startID, "StartState", StartStateType)
@@ -127,6 +121,49 @@ object parseGraph {
 
         // TODO werden die transitions richtig gebuildet?
         states(edge.start).addTransition(Transition(edge.text, s, edge.end))
+      }
+    }
+  }
+
+  /**
+   * This class holds a state while it is in creation, so it is possible
+   * to add transitions
+   */
+  private class StateCreator(val id: StateID,
+                             val name: SubjectName,
+                             val stateType: StateType) {
+    private val transitions = new ArrayBuffer[Transition]
+
+    def addTransition(transition: Transition) {
+      transitions += updateMessageType(transition)
+    }
+
+    def createState: State = State(id, name, stateType, transitions.toArray)
+
+    /**
+     * Updates the MessageType of a transition, but only if this
+     * StateCreator is for a Send- or ReceiveState
+     */
+    private def updateMessageType(transition: Transition): Transition = {
+      def transitionWithNewMessageType: Transition = {
+        val oldMessageType = transition.messageType
+        if (!messageMap.contains(oldMessageType)) {
+          System.err.println("Cant update MessageType") // TODO log error
+          return transition
+        }
+
+        Transition(messageMap(oldMessageType), transition.subjectID, transition.successorID)
+      }
+      stateType match {
+        case ReceiveStateType => {
+          transitionWithNewMessageType
+        }
+        case SendStateType => {
+          transitionWithNewMessageType
+        }
+        case _ => {
+          transition
+        }
       }
     }
   }
