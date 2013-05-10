21d9235631f27dfc9c14a282bc7c96204fa16800
Nam Truong Le <lenamtruong@gmail.com>
2013-05-10 10:43:14 +0200

    SBPM-486 solution for chapter 4

---
+++ exercises/4/solution.truong.scala
@@ -0,0 +1,122 @@
+object Solution extends App {
+
+  // execute all tasks
+  Tasks.execute()
+
+  // execute only a single one
+  //TaskManager.execute("Task 1")
+}
+
+abstract class Task(val name: String) {
+  Tasks add this
+
+  def solution()
+
+  def execute() {
+    println(name + ":")
+    solution()
+    println("\n")
+  }
+}
+
+class Tasks {
+  private var tasks = Seq[Task]()
+  def add(t: Task) = { tasks :+= t }
+  def execute() = { tasks.foreach((t: Task) => { t.execute() }) }
+  def execute(name: String) = { tasks.filter(_.name == name).head.execute() }
+}
+
+/* insert your solutions below */
+
+object Tasks extends Tasks {
+
+  new Task("Task 1") {
+    def solution() = {
+      val map = scala.collection.mutable.Map("Product1" -> 10.0, "Product2" -> 20.0, "Product3" -> 30.0)
+      println(map.mkString("\n"))
+      for ((k, v) <- map) {
+        map(k) = v * 0.9
+      }
+      println(map.mkString("\n"))
+    }
+  }
+
+  new Task("Task 2") {
+    def solution() = {
+      val in = new java.util.Scanner(new java.io.File("myfile.txt"))
+      val words = new scala.collection.mutable.HashMap[String, Int]
+      while (in.hasNext()) {
+        val w = in.next();
+        val oldCount = words.getOrElse(w, 0);
+        words(w) = oldCount + 1
+      }
+      in.close()
+      println(words.mkString("\n"))
+    }
+  }
+
+  new Task("Task 3") {
+    def solution() = {
+
+      // your solution for task 3 here
+
+    }
+  }
+
+  new Task("Task 4") {
+    def solution() = {
+
+      // your solution for task 4 here
+
+    }
+  }
+
+  new Task("Task 5") {
+    def solution() = {
+
+      // your solution for task 5 here
+
+    }
+  }
+
+  new Task("Task 6") {
+    def solution() = {
+
+      // your solution for task 6 here
+
+    }
+  }
+
+  new Task("Task 7") {
+    def solution() = {
+
+      // your solution for task 7 here
+
+    }
+  }
+
+  new Task("Task 8") {
+    def solution() = {
+
+      // your solution for task 8 here
+
+    }
+  }
+
+  new Task("Task 9") {
+    def solution() = {
+
+      // your solution for task 9 here
+
+    }
+  }
+
+  new Task("Task 10") {
+    def solution() = {
+
+      // your solution for task 10 here
+
+    }
+  }
+
+}
