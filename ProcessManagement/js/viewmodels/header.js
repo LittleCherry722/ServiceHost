c1be2d8cd67095297c1e0139286705fd9610bf7d
Arne Link <mail@arne-link.de>
2013-05-21 11:30:35 +0200

    SBPM-504 Added chardinjs overview to navigation, header and dashboard and added a trigger (? in header)

--- ProcessManagement/js/viewmodels/header.js
+++ ProcessManagement/js/viewmodels/header.js
@@ -28,6 +28,34 @@ define([
 		this.currentUser = App.currentUser;
 
 		this.oauth2callback = oauthLogin;
+
+		this.showHelp = showApplicationOverview;
+	}
+
+	// Show an overview of the application.
+	// Displays help for whatever page is currently enabled, plus
+	// a basic overview of the site including header information etc. when
+	// the home view is the currently active page.
+	var showApplicationOverview = function() {
+
+		// if the dashboard is currently not displayed (no element with id
+		// 'dashboard' found), display chardin just for the current main html
+		// content, otherwise display for the whole page (to include navigation and
+		// header).
+		if( $('#dashboard').is(':visible') ) {
+			var mainHTML = $('#main').html()
+			$('#main').html("");
+			$('#main_menu').addClass('spaced');
+			setTimeout(function() {
+				$('body').chardinJs('start')
+				$( document ).one( 'chardinJs:stop', function() {
+					$('#main_menu').removeClass('spaced');
+					$('#main').html( mainHTML ).chardinJs('start');
+				})
+			}.bind(this), 200);
+		} else if( $('#main [data-chardin-intro]').length ) {
+			$('#main').chardinJs('start')
+		}
 	}
 
 	var oauthLogin = function() {
