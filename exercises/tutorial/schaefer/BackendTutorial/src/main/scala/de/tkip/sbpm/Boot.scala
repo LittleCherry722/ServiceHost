47f9228c1685f7ccdf9087e300ed9a32474d3d53
Frederick Schäfer <fscheepy@gmx.de>
2013-06-04 03:35:32 +0200

    SBPM-552 - subfolder exercises/schaefer added

---
+++ exercises/tutorial/schaefer/BackendTutorial/src/main/scala/de/tkip/sbpm/Boot.scala
@@ -0,0 +1,23 @@
+package de.tkip.sbpm
+
+import akka.actor.Props
+import spray.can.server.SprayCanHttpServerApp
+import de.tkip.sbpm.ActorLocator._
+import de.tkip.sbpm.application.ProcessInstanceActor
+import de.tkip.sbpm.rest.FrontendInterfaceActor
+import de.tkip.sbpm.model.TestData._
+import de.tkip.sbpm.model.TestData
+
+object Boot extends App with SprayCanHttpServerApp {
+
+  val frontendInterfaceActor =
+    system.actorOf(Props[FrontendInterfaceActor], frontendInterfaceActorName)
+  val processInstanceActor =
+    system.actorOf(Props(new ProcessInstanceActor(1)), processInstanceActorName)
+
+  // create a new HttpServer using our handler tell it where to bind to
+  newHttpServer(frontendInterfaceActor) !
+    Bind(interface = "localhost", port = 8080)
+
+  println("Http server started...")
+}
