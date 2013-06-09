918ce7c3f394b9cc23af907e908f5106dfec499e
André Wagner <andre.wagner@stud.tu-darmstadt.de>
2013-06-10 00:39:33 +0200

    SBPM-567

--- Backend/project/Build.scala
+++ Backend/project/Build.scala
@@ -11,24 +11,32 @@ object ProjectBuild extends Build {
       name := "S-BPM Groupware",
       organization := "TU Darmstadt Telecooperation Group",
       version := "1.2",
-      scalaVersion := "2.10.0",
-      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      resolvers += "spray repo" at "http://repo.spray.io",
-      resolvers += "Nexus Releases" at "http://nexus.thenewmotion.com/content/repositories/releases-public/",
-      resolvers += "Sonatype Releases"  at "http://oss.sonatype.org/content/repositories/releases",
-      resolvers += "google-api-services" at "http://google-api-client-libraries.appspot.com/mavenrepo",
+      scalaVersion := "2.10.1",
+      resolvers ++= Seq(
+        "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+        "spray repo" at "http://repo.spray.io",
+        // "Nexus Releases" at "http://nexus.thenewmotion.com/content/repositories/releases-public/",
+        "Sonatype Releases"  at "http://oss.sonatype.org/content/repositories/releases",
+        "google-api-services" at "http://google-api-client-libraries.appspot.com/mavenrepo"
+      ),
       libraryDependencies ++= Seq(
-        "org.scala-lang"     % "scala-actors"   % "2.10.0",
+        // "org.scala-lang"     % "scala-actors"   % "2.10.0",
         "org.scala-lang"     % "scala-reflect"  % "2.10.0",
-        "com.typesafe.akka"  %% "akka-actor"    % "2.1.2",
-        "com.typesafe.akka"  %% "akka-testkit"  % "2.1.2"   % "test",
-        "com.typesafe.slick" %% "slick"         % "1.0.0",
-        "com.typesafe.slick" %% "slick-testkit" % "1.0.0"   % "test",
+        // "com.typesafe.akka"  %% "akka-actor"    % "2.1.2",
+        "com.typesafe.akka"  %% "akka-actor"    % "2.1.4",
+        // "com.typesafe.akka"  %% "akka-testkit"  % "2.1.2"   % "test",
+        "com.typesafe.akka"  %% "akka-testkit"  % "2.1.4"   % "test",
+        // "com.typesafe.slick" %% "slick"         % "1.0.0",
+        "com.typesafe.slick" %% "slick"         % "1.0.1",
+        // "com.typesafe.slick" %% "slick-testkit" % "1.0.0"   % "test",
+        "com.typesafe.slick" %% "slick-testkit" % "1.0.1"   % "test",
         "org.xerial"         %  "sqlite-jdbc"   % "3.7.2",
-        "junit"              %  "junit"         % "4.5"     % "test",
+        // "junit"              %  "junit"         % "4.5"     % "test",
         "org.scalatest"      %% "scalatest"     % "1.9.1"   % "test",
         "io.spray"           %  "spray-can"     % "1.1-M7",
-        "com.mchange"        %  "c3p0"          % "0.9.2",
+        // "com.mchange"        %  "c3p0"          % "0.9.2",
+        "com.mchange"        %  "c3p0"          % "0.9.2.1",
+
         "io.spray"           %  "spray-routing" % "1.1-M7",
         "io.spray"           %  "spray-testkit" % "1.1-M7"  % "test",
         "io.spray"           %% "spray-json"    % "1.2.3",
@@ -41,7 +49,7 @@ object ProjectBuild extends Build {
         "com.google.http-client"     % "google-http-client-jackson2"  % "1.13.1-beta",
         "com.google.code.findbugs"   % "jsr305"                       % "1.3.9",
         "com.google.apis"            % "google-api-services-oauth2"   % "v2-rev30-1.13.2-beta"
-        )
+      )
     )
   )
   
