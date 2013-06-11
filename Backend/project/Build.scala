52f582b0c5ce8f6f20e87a56b4fb8616865c7a4f
Stephan Spielmann <baba@baba-desktop.(none)>
2013-06-11 09:11:00 +0200

    SBPM-568 two commas were missing

--- Backend/project/Build.scala
+++ Backend/project/Build.scala
@@ -42,11 +42,11 @@ object ProjectBuild extends Build {
         // "io.spray"           %% "spray-json"    % "1.2.3",
 		"io.spray"           %% "spray-json"    % "1.2.5",
         // "ua.t3hnar.bcrypt"   %% "scala-bcrypt"  % "2.0",
-		"com.github.t3hnar" % "scala-bcrypt_2.10" % "2.1"
+		"com.github.t3hnar" % "scala-bcrypt_2.10" % "2.1",
         // "com.fasterxml.jackson.core" % "jackson-core"                 % "2.0.5",
 		"com.fasterxml.jackson.core" % "jackson-core"                 % "2.2.0",
         // "com.google.guava"           % "guava"                        % "13.0",
-		"com.google.guava"           % "guava"                        % "14.0.1"
+		"com.google.guava"           % "guava"                        % "14.0.1",
         // "com.google.oauth-client"    % "google-oauth-client-java6"    % "1.13.1-beta",
 		"com.google.oauth-client"    % "google-oauth-client-java6"    % "1.15.0-rc",
         // "com.google.api-client"      % "google-api-client-java6"      % "1.13.2-beta",
