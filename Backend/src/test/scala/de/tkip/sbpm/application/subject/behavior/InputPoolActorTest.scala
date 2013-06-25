4b06ee35d7269002417ff938c0bd4ff3fa4773b3
Matthias Jahn <matthias.jahn@stud.tu-darmstadt.de>
2013-06-25 13:03:43 +0200

    IP tests updated

--- Backend/src/test/scala/de/tkip/sbpm/application/subject/behavior/InputPoolActorTest.scala
+++ Backend/src/test/scala/de/tkip/sbpm/application/subject/behavior/InputPoolActorTest.scala
@@ -2,24 +2,24 @@ package de.tkip.sbpm.application.subject.behavior
 
 import org.scalatest.FunSuite
 import akka.testkit.{TestKit, TestActorRef}
-import akka.actor.ActorSystem
-import scala.io.Source
-import de.tkip.sbpm.rest.GraphJsonProtocol._
+import akka.actor._
+import akka.actor.ActorDSL._
 import de.tkip.sbpm.application.subject.SubjectData
-import de.tkip.sbpm.model.Graph
-import de.tkip.sbpm.application.miscellaneous.parseGraph
-import spray.json._
+import de.tkip.sbpm.model.Subject
 
 class InputPoolActorTest extends TestKit(ActorSystem("TestSystem")) with FunSuite {
 
-  val simpleGraphSource = Source.fromURL(getClass.getResource("/de/tkip/sbpm/application/miscellaneous/simple-graph.json")).mkString
-  val domainGraph = simpleGraphSource.asJson.convertTo[Graph](graphJsonFormat)
-  val graph = parseGraph(domainGraph)
-  val subjectData = SubjectData(1, 1, 1, null, null, graph.subjects("Subj1"))
+  val subject = Subject("Subj1", -1, Array(), false)
+  val subjectData = SubjectData(1, 1, 1, null, null, subject)
 
   test("register single subscriber") {
     val actor = TestActorRef(new InputPoolActor(subjectData))
+    implicit val i = inbox()
 
     actor ! SubscribeIncomingMessages(2, "other", "test")
+
+    assert(i.receive() === InputPoolSubscriptionPerformed)
   }
+
+
 }
