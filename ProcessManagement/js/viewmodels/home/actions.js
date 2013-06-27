9f99432351efa1eaf053b9c3f87efb19f48038d9
Andre Schubert <a.schubert@stud.tu-darmstadt.de>
2013-06-27 15:28:02 +0200

    SBPM-589, SBPM-590, SBPM-591 Alles erledigt.

--- ProcessManagement/js/viewmodels/home/actions.js
+++ ProcessManagement/js/viewmodels/home/actions.js
@@ -7,22 +7,24 @@ define([
 ], function( ko, App, _, Actions, Process ) {
 
 	var ViewModel = function() {
-            self.actions = Actions.all;
+            //this.actions = ko.observableArray(Actions.all());
             
+            this.actions = ko.computed(function() {return Actions.all()});                  
             this.filterProcess = ko.observable(1);
             this.filterUser = ko.observable(1);
-	}
+	};
 
-	var initialize = function( ) {
+	var initialize = function() {
             var viewModel = new ViewModel();
             
-            App.loadTemplate( "home/actions", viewModel, "executionContent", function() { });            
-	}
-
+            App.loadTemplate( "home/actions", viewModel, "executionContent", function() { });   
+            Actions.fetch();
+	};
+        
 	// Everything in this object will be the public API
 	return {
 		init: initialize
-	}
+	};
 });
 
 
