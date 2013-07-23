02738368fd90ee250e5a86a65f5f1c855db266fa
Matthias Jahn <matthias.jahn@stud.tu-darmstadt.de>
2013-07-23 18:14:08 +0200

    SBPM-694 - Akka und Spary auf neue Version aktualisieren Build.scala updated

--- Backend/project/Build.scala
+++ Backend/project/Build.scala
@@ -11,7 +11,7 @@ object ProjectBuild extends Build {
       name := "S-BPM Groupware",
       organization := "TU Darmstadt Telecooperation Group",
       version := "1.2",
-      scalaVersion := "2.10.1",
+      scalaVersion := "2.10.2",
       resolvers ++= Seq(
         "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
         "spray repo" at "http://repo.spray.io",
@@ -19,20 +19,20 @@ object ProjectBuild extends Build {
         "google-api-services" at "http://google-api-client-libraries.appspot.com/mavenrepo"
       ),
       libraryDependencies ++= Seq(
-        "org.scala-lang" % "scala-reflect" % "2.10.0",
+        "org.scala-lang" % "scala-reflect" % "2.10.2",
         "org.scalatest" %% "scalatest" % "2.0.M5b" % "test",
-        "com.typesafe.akka" %% "akka-actor" % "2.1.4",
-        "com.typesafe.akka" %% "akka-testkit" % "2.1.4" % "test",
-        "com.typesafe.akka" %% "akka-remote" % "2.1.4",
+        "com.typesafe.akka" %% "akka-actor" % "2.2.0",
+        "com.typesafe.akka" %% "akka-testkit" % "2.2.0" % "test",
+        "com.typesafe.akka" %% "akka-remote" % "2.2.0",
         "com.typesafe.slick" %% "slick" % "1.0.1",
         "com.typesafe.slick" %% "slick-testkit" % "1.0.1" % "test",
 
         "org.xerial" % "sqlite-jdbc" % "3.7.2",
         "com.mchange" % "c3p0" % "0.9.2.1",
-        "io.spray" % "spray-can" % "1.1-M7",
-        "io.spray" % "spray-routing" % "1.1-M7",
-        "io.spray" % "spray-testkit" % "1.1-M7" % "test",
-        "io.spray" %% "spray-json" % "1.2.3",
+        "io.spray" % "spray-can" % "1.2-M8",
+        "io.spray" % "spray-routing" % "1.2-M8",
+        "io.spray" % "spray-testkit" % "1.2-M8" % "test",
+        "io.spray" %% "spray-json" % "1.2.5",
         "com.github.t3hnar" % "scala-bcrypt_2.10" % "2.1",
         "com.fasterxml.jackson.core" % "jackson-core" % "2.2.0",
 
