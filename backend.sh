00804bbee4620b245acfc88b95429807a8bbbcc0
Arne Link <mail@arne-link.de>
2013-08-24 14:34:07 +0200

    kleine anpassungen des deploy scripts für den testsever

--- backend.sh
+++ backend.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 : ${SBPM_PORT:=8080}
 : ${AKKA_PORT:=2552}
-echo "Hostname:"
-echo $AKKA_HOSTNAME
+
 nohup java -jar backend.jar > log/backend_$SBPM_PORT.log 2>&1 < /dev/null &
 echo $! > pids/backend_$!.pid
