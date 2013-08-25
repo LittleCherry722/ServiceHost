47c8b33184cc5233ef7cc1198001ca37d05344ea
Andreas Rueckle <andreas.rueckle@stud.tu-darmstadt.de>
2013-08-25 17:27:54 +0200

    SBPM-780 modal graph

--- ProcessManagement/js/viewmodels/home/actions.js
+++ ProcessManagement/js/viewmodels/home/actions.js
@@ -59,54 +59,56 @@ define([
 	};
 
 	var showGraph = function(action){
-		var graphId = 'graph_bv_outer',
-			table = $( '#' + action.instanceTableId()),
-			graphContainerOuter = $('.graph', table),
-			node = 0,
-			graphContainer, processInstance, currentState, process;
 
-		$('.show-graph').removeClass('invisible');
-		$('.show-graph', table).addClass('invisible');
+		setTimeout(function(){
+			var table = $( '#' + action.instanceTableId()),
+				node = 0,
+				graphContainer = $('#graph_bv_outer'),
+				graphModal = $('#graphModal'),
+				processInstance, currentState, process;
 
-		// create graph containers
-		$('#' + graphId).remove();
-		graphContainer = $('<div/>').attr('id', graphId).appendTo(graphContainerOuter);
-
-		// fetch process instance
-		_.each( ProcessInstances.all(), function (element) {
-			if( element.id () === action.processInstanceID() ) {
-				processInstance = element;
-			}
-		});
-
-		// load graph
-		gf_loadGraph( JSON.stringify( processInstance.graph().definition ) );
-		gv_graph.selectedSubject = null;
-		gf_clickedCVnode( action.subjectID() );
-		gf_clickedCVbehavior();
-
-		// select active node
-		currentState = processInstance.getCurrentState( action.subjectID() );
-		process = processInstance.getCurrentProcess( action.subjectID() );
-
-		if( process !== null ) {
-			$.each( process.macros[0].nodes, function( i, value ) {
-				if ( value.id === currentState ) {
-					node = i;
+			// fetch process instance
+			_.each( ProcessInstances.all(), function (element) {
+				if( element.id () === action.processInstanceID() ) {
+					processInstance = element;
 				}
-			} );
+			});
 
-			if( gv_objects_nodes[node] ){
-				gf_deselectNodes();
-				gv_objects_nodes[node].select();
+			// load graph
+			gf_loadGraph( JSON.stringify( processInstance.graph().definition ) );
+			gv_graph.selectedSubject = null;
+			gf_clickedCVnode( action.subjectID() );
+			gf_clickedCVbehavior();
+
+			// select active node
+			currentState = processInstance.getCurrentState( action.subjectID() );
+			process = processInstance.getCurrentProcess( action.subjectID() );
+
+			if( process !== null ) {
+				$.each( process.macros[0].nodes, function( i, value ) {
+					if ( value.id === currentState ) {
+						node = i;
+					}
+				} );
+
+				if( gv_objects_nodes[node] ){
+					gf_deselectNodes();
+					gv_objects_nodes[node].select();
+				}
 			}
-		}
+		}, 100);
 	};
 
 	var initialize = function() {
 		var viewModel = new ViewModel();
 
-		App.loadTemplate( "home/actions", viewModel, "executionContent", function() { });
+		App.loadTemplate( "home/actions", viewModel, "executionContent", function() {
+			$('.show-graph').fancybox({
+				scrolling: 'yes',
+				transitionIn: 'none',
+				transitionOut: 'none'
+			});
+		});
 		Actions.fetch();
 	};
 
