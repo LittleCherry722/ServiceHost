7df0d76831f113aad4ff9d337b1939b55b7e5f15
diwu <wu.di1983@hotmail.com>
2013-09-05 19:56:49 +0800

    SBPM-784 sending process- and action-changes to FE fixed

--- Backend/src/main/scala/de/tkip/sbpm/rest/ChangeInterfaceActor.scala
+++ Backend/src/main/scala/de/tkip/sbpm/rest/ChangeInterfaceActor.scala
@@ -13,43 +13,35 @@ import de.tkip.sbpm.application.change._
 import spray.json._
 import de.tkip.sbpm.model._
 import akka.pattern.ask
-import scala.util.{Success,Failure}
+import scala.util.{ Success, Failure }
 
-class ChangeInterfaceActor extends Actor with HttpService with DefaultLogging{
-  
+class ChangeInterfaceActor extends AbstractInterfaceActor with DefaultLogging {
+
   import context.dispatcher
   implicit val timeout = Timeout(15 seconds)
   private lazy val processManagerActor = ActorLocator.processManagerActor
   private lazy val changeActor = ActorLocator.changeActor
   def actorRefFactory = context
-  
-  
-  def receive = runRoute {
+
+  def routing = runRoute {
     get {
       // frontend request
       pathPrefix("") {
-        parameter("since") { (time) => ctx =>
-          log.debug(s"${getClass.getName} received polling request with timestemp: $time")
-          (processManagerActor ? GetHistorySince(time.toLong)).mapTo[String].onComplete {
-            case Success(history) => {
-              (changeActor ? GetProcessChange(time.toLong)).mapTo[Option[ProcessRelatedChange]].onComplete {
-                case Success(process) => {
-                  (changeActor ? GetActionChange(time.toLong)).mapTo[Option[ActionRelatedChange]].onComplete {
-                    case Success(action) => {
-                      val result = ChangeRelatedData(process,action)
-                      complete(ChangeRelatedData(process, action))
-                    }
-                    case Failure(e) => complete(e) 
-                  }  
-                  
-                }
-                case Failure(e) => complete(e)
-              }
+        parameter("since") { (time) =>
+            complete {
+              //          log.debug(s"${getClass.getName} received polling request with timestemp: $time")
+              val future = 
+                for {
+//                  history <- (processManagerActor ? GetHistorySince(time.toLong)).mapTo[String]
+                  process <- (changeActor ? GetProcessChange(time.toLong)).mapTo[Option[ProcessRelatedChange]]
+                  action <- (changeActor ? GetActionChange(time.toLong)).mapTo[Option[ActionRelatedChange]]
+                
+                  result = ChangeRelatedData(process, action)	  
+                } yield result
+                
+                future.map(result => result)
             }
-            case Failure(e) => complete(e)
-          }
-     
-        }
+            }
       }
     }
   }
