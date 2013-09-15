00410e6b340bec4e64d3ca319bc8827cdd6aaf6d
Andreas Rueckle <andreas.rueckle@stud.tu-darmstadt.de>
2013-09-15 18:55:05 +0200

    SBPM-808 incl. execution api

--- ProcessManagement/js/viewmodels/console.js
+++ ProcessManagement/js/viewmodels/console.js
@@ -2,12 +2,16 @@ define([
 	"knockout",
 	"app",
 	"underscore",
-	"text!api_description.json"
-], function( ko, App, _, apiDescription ) {
+	"text!api_description.json",
+	"rainbow",
+	"js_beautify"
+], function( ko, App, _, apiDescription, Rainbow, js_beautify ) {
 
 
 	var ViewModel = function() {
 		var that = this;
+
+		this.outputText = ko.observable("");
 
 		this.selectedMethod = ko.observable();
 
@@ -15,13 +19,45 @@ define([
 
 		this.methodSelected = function(method){
 			that.selectedMethod(method);
-		}
+			that.outputText("");
+		};
+
+		this.executeRequest = function(method){
+			var viewElement = $('#console'),
+				url = method.url;
+
+			// replace url parameters
+			_.each(url.match(/(\{\w*\})/ig), function(match){
+				var elementName = match.replace(/\{(\w*)\}/ig, "$1"),
+					val = $('.parameters-list input[name="'+elementName+'"]', viewElement).val();
+
+				url = url.replace(match, val)
+			});
+
+			$.ajax({
+				url: url,
+				type: method.method,
+				dataType: 'text'
+			}).always(function(data){
+				var text = data;
+				if(typeof  data === 'object' && 'responseText' in data){
+					text = "// Response: " + data.responseText;
+				}
+				that.outputText('// ' + method.method + ' ' + url + '\n' + text)
+			});
+
+		};
 
 	};
 
 
 	var initialize = function() {
 		var viewModel = new ViewModel();
+		viewModel.outputText.subscribe(function(data){
+			Rainbow.color(js_beautify(data), 'javascript', function(highlighted_code) {
+				$('#console .output').html(highlighted_code);
+			});
+		});
 
 		App.loadTemplate( "console", viewModel, null, function() {
 			//nothing here
