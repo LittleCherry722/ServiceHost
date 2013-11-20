4c06a698bd2c993d38a86cf560a790d9adbaa447
André Wolski <locke@dena-design.de>
2013-11-20 16:57:04 +0100

    SBPM-891 copy solution.template.scala

---
+++ exercises/10/solution.wolski.scala
@@ -0,0 +1,73 @@
+object Solution extends App {
+
+  // execute all tasks
+  Tasks.execute()
+
+  // execute only a single one
+  //TaskManager.execute("Task 1")
+}
+
+abstract class Task(val name: String) {
+  Tasks add this
+
+  def solution()
+
+  def execute() {
+    println(name + ":")
+    solution()
+    println("\n")
+  }
+}
+
+class Tasks {
+  private var tasks = Seq[Task]()
+  def add(t: Task) = { tasks :+= t }
+  def execute() = { tasks.foreach((t: Task) => { t.execute() }) }
+  def execute(name: String) = { tasks.filter(_.name == name).head.execute() }
+}
+
+/* insert your solutions below */
+
+object Tasks extends Tasks {
+
+  new Task("Task 1") {
+    def solution() = {
+
+      // your solution for task 1 here
+
+    }
+  }
+
+  new Task("Task 2") {
+    def solution() = {
+
+      // your solution for task 2 here
+
+    }
+  }
+
+  new Task("Task 4") {
+    def solution() = {
+
+      // your solution for task 4 here
+
+    }
+  }
+
+  new Task("Task 6") {
+    def solution() = {
+
+      // your solution for task 6 here
+
+    }
+  }
+
+  new Task("Task 10") {
+    def solution() = {
+
+      // your solution for task 10 here
+
+    }
+  }
+
+}
