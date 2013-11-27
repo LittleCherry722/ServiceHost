a99823fd27bd50cd96adc7c9ba12fe4d5f0e2e87
André Wolski <locke@dena-design.de>
2013-11-27 17:07:53 +0100

    SBPM-891 Aufgaben Woche 3

--- exercises/10/solution.wolski.scala
+++ exercises/10/solution.wolski.scala
@@ -31,42 +31,140 @@ class Tasks {
 object Tasks extends Tasks {
 
   new Task("Task 1") {
+    trait RectangleLike extends java.awt.geom.Ellipse2D {
+      def translate(dx: Int, dy: Int): Unit = {
+        this.setFrame(this.getX + dx, this.getY + dy, this.getWidth, this.getHeight)
+      }
+      
+      def grow(h: Int, v: Int): Unit = {
+        this.setFrame(this.getX - h, this.getY - v, this.getWidth + 2*h, this.getHeight + 2*v)
+      }
+      
+      def mkString(): String = {
+        "x="+this.getX+";y="+this.getY+";w="+this.getWidth+";h="+this.getHeight
+      }
+    }
+    
     def solution() = {
-
-      // your solution for task 1 here
-
+      val egg = new java.awt.geom.Ellipse2D.Double(5, 10, 20, 30) with RectangleLike
+      println("original: "+egg.mkString)
+      egg.translate(10, -10)
+      println("translate: "+egg.mkString)
+      egg.grow(10, 20)
+      println("grow: "+egg.mkString)
     }
   }
 
   new Task("Task 2") {
+    class OrderedPoint(val orderedX: Int, val orderedY: Int) extends java.awt.Point(orderedX, orderedY) with scala.math.Ordered[java.awt.Point] {
+      
+      def compare(that: java.awt.Point): Int = {
+        this.compare(this.asInstanceOf[java.awt.Point], that.asInstanceOf[java.awt.Point])
+      }
+      
+      
+      def compare(that: OrderedPoint): Int = {
+        this.compare(that.asInstanceOf[java.awt.Point])
+      }
+      
+      def compare(self: java.awt.Point, that: java.awt.Point): Int = {
+        if (this.getX < that.getX || (this.getX == that.getX && this.getY < that.getY)) {
+          -1
+        }
+        else if (this.getX == that.getX && this.getY == that.getY) {
+          0
+        }
+        else {
+          1
+        }
+      }
+      /*
+      def <(that: java.awt.Point): Boolean = {
+        this.compare(that) < 0
+      }
+      */
+    }
+    
     def solution() = {
-
-      // your solution for task 2 here
-
+      import java.awt.Point
+      
+      val a1 = new OrderedPoint(-1, -1)
+      println("a1: " + a1);
+      
+      val a2 = new OrderedPoint(2, -1)
+      println("a2: " + a2);
+      
+      val a3 = new OrderedPoint(-2, 1)
+      println("a3: " + a3);
+      
+      println("a1 compare a2: " + a1.compare(a2))
+      println("a1 < a2:" + (a1 < a2))
+      
+      println("a1 compare a3: " + a1.compare(a3))
+      println("a1 < a3:" + (a1 < a3))
+      
+      // TODO: this fails
+      val x = Array(new OrderedPoint(-1, -1), new OrderedPoint(0, 0), new OrderedPoint(1, 1))
+      //scala.util.Sorting.quickSort(x)
+      println("x: "+x.mkString)
     }
   }
 
   new Task("Task 4") {
+    class CryptoLogger(val key: Int = 3) {
+      private val buffer = new StringBuffer
+      private def encrypt(msg: String) = for (c <- msg) yield (c+key).toChar
+      def log(msg: String) = this.buffer.append(this.encrypt(msg+"\n"))
+      override def toString = this.buffer.toString
+    }
     def solution() = {
-
-      // your solution for task 4 here
-
+      val logger0 = new CryptoLogger(0)
+      val logger1 = new CryptoLogger
+      val logger2 = new CryptoLogger(-3)
+      
+      logger0.log("start..")
+      logger0.log("Hallo verschlüsselte Welt")
+      println("logger0: " + logger0)
+      
+      logger1.log("start..")
+      logger1.log("Hallo verschlüsselte Welt")
+      println("logger1: " + logger1)
+      
+      logger2.log(logger1.toString)
+      println("logger2: " + logger2)
     }
   }
 
   new Task("Task 6") {
     def solution() = {
 
-      // your solution for task 6 here
+      println("As Container extends Component, JContainer should extend JComponent, but also Container. However, a Java class can only extend one other class.")
+      println("In Scala you could choose JComponent to be a trait and define JContainer 'with' it.")
 
     }
   }
 
   new Task("Task 10") {
+    class IterableInputStreamIterator(val stream: java.io.InputStream) extends Iterator[Byte] {
+      def next: Byte = {stream.read().asInstanceOf[Byte]}
+      def hasNext: Boolean = stream.available > 0
+    }
+    
+    trait IterableInputStream extends java.io.InputStream with Iterable[Byte] {
+      def iterator(): Iterator[Byte] = new IterableInputStreamIterator(this)
+    }
+
     def solution() = {
+      class IterableFileInputStream(filename: String) extends java.io.FileInputStream(filename) with IterableInputStream;
 
-      // your solution for task 10 here
+      val file: IterableInputStream = new IterableFileInputStream("solution.wolski.scala")
+      // uncomment next line for demonstration
+      //for (byte <- file) print(byte.toChar);
 
+
+      // this would not work
+      //val file = new java.io.FileInputStream("solution.wolski.scala")
+      //for (byte <- file) print(byte.toChar);
     }
   }
 
--- exercises/11/solution.wolski.scala
+++ exercises/11/solution.wolski.scala
@@ -32,25 +32,98 @@ object Tasks extends Tasks {
 
   new Task("Task 1") {
     def solution() = {
-
-      // your solution for task 1 here
+      
+      println("3 + 4 -> 5 is evaluated as ((3 + 4) -> 5)")
+      println("3 -> 4 + 5 is evaluated as ((3 -> 4) + 5)")
 
     }
   }
 
   new Task("Task 4") {
+    class Money(val amount: Int) {
+      def this(dollars: Int, cents: Int) {
+        this(dollars*100 + cents)
+      }
+
+
+      def +(other: Money): Money = { new Money(this.amount + other.amount) }
+      def -(other: Money): Money = { new Money(this.amount - other.amount) }
+      def ==(other: Money): Boolean = { this.amount == other.amount }
+      def <(other: Money): Boolean = { this.amount < other.amount }
+      
+      override def toString: String = {
+        val dollars = this.amount / 100
+        val cents = math.abs(this.amount % 100)
+        val cents_str = if (cents < 10) "0"+cents else cents
+        dollars+"."+cents_str+" $"
+      }
+    }
+
+    object Money {
+      def apply(dollars: Int, cents: Int) = { new Money(dollars, cents) }
+    }
+
     def solution() = {
+      /*
+      var m1 = new Money(1, 0)
+      var m2 = new Money(-2, 0)
+      var m3 = new Money(0, 50)
+      var m4 = new Money(0, 1)
 
-      // your solution for task 4 here
+      println("m1: " + m1);
+      println("m1 + m2: " + (m1 + m2));
+      println("m1: " + m1);
+      m1 += m2
+      println("m1 += m2: " + m1);
 
+      println("")
+      println("m3: " + m3)
+      println("m3 + m3: " + (m3+m3))
+      println("m3 + m3 + m3: " + (m3+m3+m3))
+      println("m3: " + m3)
+      
+      println("m4: " + m4)
+
+      println("m1-m2 == m3+m3: " + ((m1-m2)==(m3+m3)));
+      println("m4 < m3: " + (m4<m3));
+      */
+
+      println("(Money(1, 75) + Money(0, 50) == Money(2, 25)): " + (Money(1, 75) + Money(0, 50) == Money(2, 25)))
+
+      println("I would not provide (Money*Money->Money), as money can not be multiplied with money. (Money*Double->Money) would make sense, therefore also (Money/Money->Double)");
     }
   }
 
   new Task("Task 7") {
+    class BitSequence {
+       var data: Long = 0
+       val zero: Long = 0
+       val one: Long = 1
+       val not_one: Long = -2
+       
+       def apply(n: Int): Boolean = {
+         (this.data & (one<<n)) != zero
+       }
+
+       def update(n: Int, value: Boolean): Unit = {
+         if (value) { this.data |= (one<<n) } else { this.data &= (not_one<<n) }
+       }
+
+       override def toString = this.data.toBinaryString
+    }
+
     def solution() = {
-
-      // your solution for task 7 here
-
+      val seq = new BitSequence
+      println("seq: "+seq)
+      seq.update(0, true)
+      println("seq: "+seq)
+      seq.update(2, true)
+      println("seq: "+seq)
+      seq.update(0, false)
+      println("seq: "+seq)
+      seq.update(40, true)
+      println("seq: "+seq)
+      println("seq(2): "+seq(2))
     }
   }
 
