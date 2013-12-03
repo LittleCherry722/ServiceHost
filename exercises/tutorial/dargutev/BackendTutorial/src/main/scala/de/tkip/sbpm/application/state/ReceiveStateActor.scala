9b8f248b3718739648b3ca229bcde0d251360c7e
Sheip Dargutev <sheip_dargutev@yahoo.com>
2013-12-03 16:32:53 +0100

    SBPM-903 Aufgaben Woche 5

---
+++ exercises/tutorial/dargutev/BackendTutorial/src/main/scala/de/tkip/sbpm/application/state/ReceiveStateActor.scala
@@ -0,0 +1,17 @@
+package de.tkip.sbpm.application.state
+
+import de.tkip.sbpm.model.State
+import de.tkip.sbpm.application._
+
+class ReceiveStateActor(s: State) extends AbstractBeviorStateActor(s) {
+  def receive = {
+    case SubjectToSubjectMessage(_, _, content) => {
+      println("ReceiveState[%s] received: " + content)
+      sender ! Ack
+    }
+    case ExecuteAction(_, succ) if (s.transitions contains succ) => {
+      context.parent ! ChangeState(s.transitions(s.transitions.indexOf(succ)))
+    }
+    // TODO implement ExecuteAction
+  }
+}
---
+++ exercises/tutorial/dargutev/BackendTutorial/src/main/scala/de/tkip/sbpm/application/state/SendStateActor.scala
@@ -0,0 +1,23 @@
+package de.tkip.sbpm.application.state
+
+import de.tkip.sbpm.model.State
+import de.tkip.sbpm.application.ExecuteAction
+import de.tkip.sbpm.model.TestData
+import de.tkip.sbpm.application.SubjectToSubjectMessage
+import de.tkip.sbpm.application.ChangeState
+
+class SendStateActor(s: State) extends AbstractBeviorStateActor(s) {
+  def receive = {
+    case ExecuteAction(id, succ) => {
+      if (TestData(2).subject1.subjectID == id) {
+        context.parent ! SubjectToSubjectMessage(id, TestData(2).subject2.subjectID, "data")
+      } else {
+        context.parent ! SubjectToSubjectMessage(TestData(2).subject2.subjectID, id, "data")
+      }
+      context.parent ! ChangeState(s.transitions(s.transitions.indexOf(succ)))
+    }
+    // TODO implement ExecuteAction
+    case _ =>
+  }
+}
+ 
