17a0ffe9ddbb95d639e0212a835e05d3497086b2
Patrick Rauscher <prauscher@ohai.su>
2013-12-15 23:44:40 +0100

    SBPM-904 stuff.

---
+++ exercises/tutorial/rauscher/BackendTutorial/src/main/scala/de/tkip/sbpm/application/state/SendStateActor.scala
@@ -0,0 +1,11 @@
+package de.tkip.sbpm.application.state
+
+import de.tkip.sbpm.model.State
+
+class SendStateActor(s: State) extends AbstractBeviorStateActor(s) {
+  def receive = {
+    // TODO implement ExecuteAction
+    case _ =>
+  }
+}
+ 
