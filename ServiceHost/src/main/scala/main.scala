f0617cac64633ad5ad69b53b6cbc6728f2aa6a4c
André Wolski <locke@dena-design.de>
2014-02-19 12:52:50 +0100

    SBPM-929: receive SubjectToSubjectMessage from 'Großunternehmen'

--- ServiceHost/src/main/scala/main.scala
+++ ServiceHost/src/main/scala/main.scala
@@ -3,6 +3,13 @@ import scalaj.http.{Http, HttpOptions}
 import Messages.RegisterServiceMessage
 import Messages.ExecuteServiceMessage
 
+import java.util.Date
+import de.tkip.sbpm.application.miscellaneous._
+import de.tkip.sbpm.application.subject.behavior._
+import de.tkip.sbpm.application.miscellaneous.ProcessAttributes._
+import de.tkip.sbpm.application.subject.misc._
+
+
 /*
 
 Momentan funktioniert es nur so: Starte Instanz von Prozess Großunternehmen. Führe aus bis send. Message kommt hier an.
@@ -50,17 +57,63 @@ object main extends App {
 
 
 class ServiceHostActor extends Actor {
+  private var userId = 0
+  private var processId = 0
+
   def receive: Actor.Receive = {
     case register: RegisterServiceMessage => {
       println("received RegisterServiceMessage: " + register)
       // TODO implement
+      sender ! Some("some RegisterServiceMessage answer")
     }
     case execute: ExecuteServiceMessage => {
       println("received ExecuteServiceMessage: " + execute)
       // TODO implement
+      sender ! Some("some ExecuteServiceMessage answer")
+    }
+    case request: CreateProcessInstance => {
+      println("received CreateProcessInstance: " + request)
+      userId = request.userID
+      processId = request.processID
+      // TODO implement
+
+      // fake ProcessInstanceActor:
+
+      val persistenceGraph = null
+      val processName = ""
+      val startedAt = new Date()
+      val actions = null
+      val processInstanceData = ProcessInstanceData(0, request.name, request.processID, processName, persistenceGraph, false, startedAt, request.userID, actions)
+      sender ! ProcessInstanceCreated(request, self, processInstanceData)
+    }
+    case GetProxyActor => {
+      println("received GetProxyActor")
+      // TODO implement
+      // fake ProcessInstanceProxyActor:
+      sender ! self
+    }
+    case message: SubjectToSubjectMessage => {
+      println("got SubjectToSubjectMessage " + message + " from " + sender)
+      // TODO implement
+
+      // fake InputPoolActor:
+
+      // Unlock the sender
+      sender ! Stored(message.messageID)
+      println("unblocked sender")
+
+      // TODO store and handle the message
+      val target = Target("Großunternehmen",0,1,false,None,true,true)
+      val messageType = "Message Type"
+      val messageContent = "Message Content"
+      val answer = SubjectToSubjectMessage(0, processId, userId, "Staples", target, messageType, messageContent)
+      //val to_actor = TODO
+      //println("send " + answer + " to " + to_actor)
+      //to_actor ! answer
     }
     case something => {
       println("received something: "+something)
+      sender ! Some("some answer")
     }
   }
 }
