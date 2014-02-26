fafd21a2d71a87a8386767c98eecb35e0180babc
dle126 <ledan1206@gmail.com>
2014-02-26 17:45:29 +0100

    find class reference from name

--- ServiceHost/src/main/scala/de/tkip/servicehost/ReferenceXMLActor.scala
+++ ServiceHost/src/main/scala/de/tkip/servicehost/ReferenceXMLActor.scala
@@ -5,6 +5,8 @@ import de.tkip.servicehost.Messages._
 import de.tkip.servicehost.serviceactor._
 
 class ReferenceXMLActor extends Actor{
+  val packet = "de.tkip.servicehost.serviceactor"
+    //TODO create mapping from XML
   
   def receive: Actor.Receive = {
     case createReference: CreateXMLReferenceMessage => {
@@ -13,7 +15,9 @@ class ReferenceXMLActor extends Actor{
     case getReference: GetClassReferenceMessage =>{
       getReference.serviceID match {
         case "Staples" =>
-        	sender ! new ClassReferenceMessage(getReference.serviceID, classOf[StaplesServiceActor])
+//        	sender ! new ClassReferenceMessage(getReference.serviceID, classOf[StaplesServiceActor])
+        	
+        	sender ! new ClassReferenceMessage(getReference.serviceID, Class.forName(packet+".StaplesServiceActor").asInstanceOf[Class[ServiceActor]])
         case _ =>
           println(getReference.serviceID)
       }
