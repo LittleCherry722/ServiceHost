2684a4a8889eb87d1e20b17ce0c1bca670471624
André Wolski <locke@dena-design.de>
2014-03-04 03:05:11 +0100

    SbpmEventGenerator: start ui only after success of resolveOne

--- Beispiel/SbpmEventGenerator/src/main/scala/de/tkip/sbpm/eventbus/MainBoots.scala
+++ Beispiel/SbpmEventGenerator/src/main/scala/de/tkip/sbpm/eventbus/MainBoots.scala
@@ -12,32 +12,38 @@ import event._
 
 object MainBoots extends App {
   val ui = new UI
-  ui.visible = true
   val system = ActorSystem("EventBus")
   val remotePubActor = system.actorSelection("akka.tcp://sbpm@127.0.0.1:2552/user/eventbus-remote-publish")
   val resolveTimeout = Duration(40000, MILLISECONDS)
   val f = remotePubActor.resolveOne(resolveTimeout)
-  var s = true
+  var remoteActor: Option[akka.actor.ActorRef] = None
+  var running = false
+  f onComplete {
+    case Success(actor) =>
+      ui.visible = true
+      remoteActor = Some(actor)
+    case Failure(e) => e.printStackTrace()
+  }
 
   remotePubActor ! "Hello from remote app"
 
   import system.dispatcher
 
   private def startSbpmEventBusTrafficFlowMessage(): Unit = {
-    Thread.sleep(1000)
-    s = true
-    f onComplete {
-      case Success(actor) =>
-        while (s) {
-          actor ! SbpmEventBusEvent("/traffic/darmstadt/flow", new SbpmEventBusTrafficFlowMessage(Random.nextInt(10), Random.nextInt(90) + 10))
-          Thread.sleep(1000)
-        }
-      case Failure(e) => e.printStackTrace()
+    running = true
+    // run the loop in a new thread
+    future {
+      Thread.sleep(1000)
+      while (running) {
+        println("sending SbpmEventBusTrafficFlowMessage")
+        remoteActor.get ! SbpmEventBusEvent("/traffic/darmstadt/flow", new SbpmEventBusTrafficFlowMessage(Random.nextInt(10), Random.nextInt(90) + 10))
+        Thread.sleep(1000)
+      }
     }
   }
 
   private def stopSbpmEventBusTrafficFlowMessage(): Unit = {
-    s = false
+    running = false
   }
 
   class UI extends MainFrame {
