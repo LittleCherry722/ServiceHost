6bc20da92f992a25b5f575016d563814732b3c86
dle126 <ledan1206@gmail.com>
2014-03-04 21:12:40 +0100

    backup add Target

--- ServiceHost/src/main/scala/de/tkip/servicehost/serviceactor/stubgen/States.scala
+++ ServiceHost/src/main/scala/de/tkip/servicehost/serviceactor/stubgen/States.scala
@@ -7,45 +7,63 @@ import de.tkip.sbpm.application.subject.behavior.Transition
 import de.tkip.sbpm.application.subject.misc._
 import de.tkip.sbpm.application.subject.behavior.state.StateData
 
+class Target(id: Int, min: Int, max: Int, createNew: Boolean, variable: Option[String]) {
+  //  def apply(id: Int, min:Int, max:Int, createNew : Boolean, variable: Option[String]){
+  val toExternal = true
+  val defaultValues = false;
+  val target = new de.tkip.sbpm.application.subject.behavior.Target(String.valueOf(id), min, max, createNew, variable, toExternal, defaultValues)
+  //}
+
+}
+object Target {
+  def apply(id: Int, min: Int, max: Int, createNew: Boolean, variable: String) = {
+    new Target(id, min, max, createNew, Option(variable))
+  }
+}
+
 abstract class State {
 
-//  var comment, conversation, macro, message, state, subject, text, typeState, variable: String
-//  var deactivated, end, majorStartNode, start: Boolean
-  var id = -1.0//, correlationId: Double
-//  var varMan: Map[String, String]
+  //  var comment, conversation, macro, message, state, subject, text, typeState, variable: String
+  //  var deactivated, end, majorStartNode, start: Boolean
+  var id = -1.0 //, correlationId: Double
+  //  var varMan: Map[String, String]
   var targetId = -1.0
-  
-//  var sourceNode:  Map[String, Any]
-  
-//  var edge : Transition
-  
-  def apply(id: Double, targetId: Double){
+  var exitType: String
+  var target: Target
+
+  //  var sourceNode:  Map[String, Any]
+
+  //  var edge : Transition
+
+  def apply(id: Double, exitType: String, target: Target, targetId: Double) {
     this.id = id;
     this.targetId = targetId
+    this.exitType = exitType
+    this.target = target
   }
-//  def apply(id: Double, targetId: Double, args: Any){
-//    this.id = id;
-//    this.targetId = targetId
-//    //TODO: args???
-//  }
-//  def apply(sourceNode: Map[String, Any], edge: Transition, targetId: Double) {
-//    id = sourceNode("id").asInstanceOf[Int];
-//    end = sourceNode("end").asInstanceOf[Boolean];
-//    start = sourceNode("start").asInstanceOf[Boolean];
-//    majorStartNode = sourceNode("majorStartNode").asInstanceOf[Boolean];
-//    typeState = sourceNode("type").asInstanceOf[String];
-//    
-//    this.targetId = targetId
-//    this.edge = edge
-//    this.sourceNode = sourceNode
-//  }
+  //  def apply(id: Double, targetId: Double, args: Any){
+  //    this.id = id;
+  //    this.targetId = targetId
+  //    //TODO: args???
+  //  }
+  //  def apply(sourceNode: Map[String, Any], edge: Transition, targetId: Double) {
+  //    id = sourceNode("id").asInstanceOf[Int];
+  //    end = sourceNode("end").asInstanceOf[Boolean];
+  //    start = sourceNode("start").asInstanceOf[Boolean];
+  //    majorStartNode = sourceNode("majorStartNode").asInstanceOf[Boolean];
+  //    typeState = sourceNode("type").asInstanceOf[String];
+  //    
+  //    this.targetId = targetId
+  //    this.edge = edge
+  //    this.sourceNode = sourceNode
+  //  }
   def process()(implicit actor: ServiceActor)
 
 }
 
 case class ReceiveState extends State {
   def process()(implicit actor: ServiceActor) {
-	  //TODO
+    //TODO
   }
 
   def handle(msg: Any)(implicit actor: ServiceActor) {
@@ -56,24 +74,21 @@ case class ReceiveState extends State {
 
 object SendState extends State {
   def process()(implicit actor: ServiceActor) {
-	val msg =""  
+    val msg = ""
     send(msg)
   }
 
   def send(msg: String)(implicit actor: ServiceActor) {
-    
+
     val messageID = 100 //TODO change if needed
-    
-    val messageType = actor.getMessageType()
-    val target = actor.getTarget()
-    
-    
+
+    val messageType = exitType
+
     val userID = actor.getUserID()
     val processID = actor.getProcessID()
     val subjectID = actor.getSubjectID()
     val sender = actor.getSender()
-    
-    
+
     val fileInfo = None
 
     sender !
@@ -82,7 +97,7 @@ object SendState extends State {
         processID,
         userID,
         subjectID,
-        target,
+        target.target,
         messageType,
         msg,
         fileInfo)
