92224653d8c0a8b3e3d40f7d8b1d99b7b0e1d23d
dle126 <ledan1206@gmail.com>
2014-03-04 19:19:29 +0100

    backup StateImpl

--- ServiceHost/src/main/scala/de/tkip/servicehost/serviceactor/stubgen/States.scala
+++ ServiceHost/src/main/scala/de/tkip/servicehost/serviceactor/stubgen/States.scala
@@ -1,15 +1,70 @@
 package de.tkip.servicehost.serviceactor.stubgen
 
-import scala.collection.immutable.List;
+import scala.collection.immutable.List
+import de.tkip.servicehost.serviceactor.ServiceActor
+import akka.actor.PoisonPill
 
-trait State {
-  
-	def apply(startState: State, endState: State, args : List[Any])	
-	
-	def process()
-	
+import de.tkip.sbpm.application.subject.behavior.Transition
+
+abstract class State {
+
+  var comment, conversation, macro, message, state, subject, text, typeState, variable: String
+  var deactivated, end, majorStartNode, start: Boolean
+  var id, correlationId: Double
+  var varMan: Map[String, String]
+
+  def apply(id: Double, nextStateId: Double, args: Any)
+  def apply(sourceNode: Map[String, Any], edge: Any, targetId: Double) {
+    id = sourceNode("id").asInstanceOf[Int];
+    end = sourceNode("end").asInstanceOf[Boolean];
+    start = sourceNode("start").asInstanceOf[Boolean];
+    majorStartNode = sourceNode("majorStartNode").asInstanceOf[Boolean];
+    typeState = sourceNode("type").asInstanceOf[String];
+  }
+  def process()(implicit actor: ServiceActor)
+
 }
 
-object ReceiveState extends State
-object SendState extends State
-object ExitState extends State
+case class ReceiveState extends State {
+  def process()(implicit actor: ServiceActor) {
+
+  }
+
+  def handle(msg: Any)(implicit actor: ServiceActor) {
+    actor.storeMsg(msg)
+    actor.changeState()
+  }
+}
+object SendState extends State {
+  def process()(implicit actor: ServiceActor) {
+
+  }
+
+  def send(msg: String, transition: Transition)(implicit actor: ServiceActor) {
+    val sender = actor.getSender()
+    val messageType = transition.messageType
+    val toSubject = transition.subjectID
+    val messageID = 100 //TODO change if needed
+    val target = transition.target.get
+    
+    val fileInfo = null
+
+    val subjectId = id
+    sender !
+      SubjectToSubjectMessage(
+        messageID,
+        processID,
+        userID,
+        subjectID,
+        target,
+        messageType,
+        msg,
+        fileInfo)
+  }
+}
+object ExitState extends State {
+
+  def process()(implicit actor: ServiceActor) {
+    actor.self ! PoisonPill
+  }
+}
