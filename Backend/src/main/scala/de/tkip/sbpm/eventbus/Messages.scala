2ff070d3ba88a64cb6fc67c846f8fe50fa122f0f
Andreas Rueckle <andreas.rueckle@stud.tu-darmstadt.de>
2014-03-10 15:21:12 +0100

    cleaning up messages

--- Backend/src/main/scala/de/tkip/sbpm/eventbus/Messages.scala
+++
@@ -1,6 +0,0 @@
-package de.tkip.sbpm.eventbus
-
-case class SbpmEventBusTextMessage(text: String)
-case class SbpmEventBusTrafficFlowMessage(sensorId: Int, count: Int)
-case class AskForTrafficJam()
-case class ReplyForTrafficJam(result: String)
--- eventbus/Messages.scala
+++ eventbus/Messages.scala
@@ -4,3 +4,4 @@ abstract class SbpmEventBusMessage
 case class SbpmEventBusTextMessage(text: String) extends SbpmEventBusMessage
 case class SbpmEventBusTrafficFlowMessage(sensorId: Int, count: Int) extends SbpmEventBusMessage
 case class AskForTrafficJam()
+case class ReplyForTrafficJam(result: String)
