LittleCherry722/ServiceHost

commit 0dafdb2c43373b11fef63a5af74559f5c31d9ff7

add logging for frontend interface actor

Backend/src/main/scala/de/tkip/sbpm/rest/FrontendInterfaceActor.scala
@@ -15,6 +15,7 @@ package de.tkip.sbpm.rest
 import akka.actor.{ ActorRef, Actor, Props }
 import spray.routing._
 import spray.http._
+import spray.http.Uri._
 import spray.client.pipelining._
 import de.tkip.sbpm.rest.auth.CookieAuthenticator
 import de.tkip.sbpm.rest.auth.SessionDirectives._
@@ -66,6 +67,8 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
   protected def configFlag(key: String) =
     context.system.settings.config.getBoolean(configPath + key)
 
+  private val traceLogger = Logging(context.system, this)
+
   private val frontendBaseUrl = configString("frontend.baseUrl")
   private val frontendIndexFile = configString("frontend.indexFile")
   private val frontendBaseDir = configString("frontend.baseDirectory")
@@ -107,7 +110,7 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
   private val messageInterfaceActor = context.actorOf(Props[MessageInterfaceActor], "message-interface")
   private val repositoryInterfaceActor = context.actorOf(Props[RepositoryInterfaceActor], "repository-interface")
 
-  def receive = runRoute({
+  private val receiver = runRoute({
     pathPrefix("BIR") {
       delegateTo(gbirInterfaceActor)
       //      post {
@@ -233,6 +236,23 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
       }
   })
 
+  def logReceive:PartialFunction[Any, Any] = {
+    case request: spray.http.HttpRequest => {
+      val path = request.uri.path
+      if(!path.startsWith(Path.SingleSlash + frontendBaseUrl)){
+        traceLogger.debug("TRACE: ====================================================================================")
+        traceLogger.debug("TRACE:  REQUEST: " + request.method + ": " + path)
+        traceLogger.debug("TRACE: ------------------------------------------------------------------------------------")
+      }
+      request
+    }
+    case something => {
+      something
+    }
+  }
+
+  def receive = logReceive andThen receiver
+
   def serveStaticFiles: Route = {
     // root folder -> redirect to frontendBaseUrl
     path("") {
@@ -253,9 +273,7 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
    * without authentication.
    */
   private def delegateTo(actor: ActorRef): Route = {
-    val traceLogger = Logging(context.system, this)
     traceLogger.debug("TRACE: from " + this.self + " to " + actor +" RequestContext")
-
     requestContext => actor ! requestContext
   }
 

Backend/src/main/scala/de/tkip/sbpm/application/ContextResolverActor.scala
@@ -65,9 +65,9 @@ class ContextResolverActor extends Actor with DefaultLogging {
       subjectInstanceMap += (processId, processInstanceId, subjectId) -> userId
 
     case ruid: RequestUserID =>
-      logger.debug("TRACE: from " + this.self + " to " + sender + " " + ruid.generateAnswer(evaluateUserID(ruid.subjectInformation)))
-
-      sender ! ruid.generateAnswer(evaluateUserID(ruid.subjectInformation))
+      val answer = ruid.generateAnswer(evaluateUserID(ruid.subjectInformation))
+      logger.debug("TRACE: from " + this.self + " to " + sender + " " + answer)
+      sender ! answer
 
     case ss => logger.error("ContextResolver not yet implemented Message: {}", ss)
   }

commit ab7066d7291a024e169e0f70cef391b7d44478c5

prevent frequent crash of FrontendInterfaceActor
runRoute seems not to handle Closed (of class akka.io.Tcp$Closed$)

(frequent crash was introduced in 0dafdb2c43)

Backend/src/main/scala/de/tkip/sbpm/rest/FrontendInterfaceActor.scala
@@ -229,7 +229,7 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
       }
   })
 
-  def logReceive:PartialFunction[Any, Any] = {
+  def logReceive(x: Any): Unit = x match {
     case request: spray.http.HttpRequest => {
       val path = request.uri.path
       if(!path.startsWith(Path.SingleSlash + frontendBaseUrl)){
@@ -237,14 +237,19 @@ class FrontendInterfaceActor extends Actor with DefaultLogging with HttpService 
         log.debug("TRACE: request " + request.method + ": " + path)
         log.debug("TRACE: -------------------------------------------------------------------------")
       }
-      request
     }
-    case something => {
-      something
-    }
+    case _ =>
   }
 
-  def receive = logReceive andThen receiver
+  def receive = {
+    case something => try {
+      logReceive(something)
+      receiver(something)
+    }
+    catch {
+      case e: Exception => log.error(e, "error running route");
+    }
+  }
 
   def serveStaticFiles: Route = {
     // root folder -> redirect to frontendBaseUrl