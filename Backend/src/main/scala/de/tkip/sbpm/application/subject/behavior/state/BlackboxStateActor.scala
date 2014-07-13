261ad846fdb2cfe93aa9d10dddbc61033746cd36
André Wolski <locke@dena-design.de>
2014-07-13 15:42:06 +0200

    BlackboxStateActor: draft working

--- Backend/src/main/scala/de/tkip/sbpm/application/subject/behavior/state/BlackboxStateActor.scala
+++ Backend/src/main/scala/de/tkip/sbpm/application/subject/behavior/state/BlackboxStateActor.scala
@@ -14,14 +14,18 @@
 package de.tkip.sbpm.application.subject.behavior.state
 
 import scala.Array.canBuildFrom
+import akka.actor.Actor
 import akka.actor.Status.Failure
 import akka.actor.actorRef2Scala
 import de.tkip.sbpm.application.miscellaneous.AnswerAbleMessage
 import de.tkip.sbpm.application.miscellaneous.MarshallingAttributes.exitCondLabel
+import de.tkip.sbpm.application.miscellaneous.MarshallingAttributes.timeoutLabel
 import de.tkip.sbpm.application.subject.behavior.Transition
 import de.tkip.sbpm.application.subject.misc.ActionData
 import de.tkip.sbpm.application.subject.misc.ActionExecuted
+import de.tkip.sbpm.application.subject.misc.AvailableAction
 import de.tkip.sbpm.application.subject.misc.ExecuteAction
+import de.tkip.sbpm.application.subject.misc.ActionIDProvider
 import akka.event.Logging
 
 case class BlackboxStateActor(data: StateData)
@@ -31,9 +35,12 @@ case class BlackboxStateActor(data: StateData)
   var internalState = "a"
   val internalStates = Array("exit", "a", "b")
 
-  protected def stateReceive = {
+  protected def stateReceive = Actor.emptyBehavior
 
-    case action: ExecuteAction => {
+  override protected def interceptReceive: Receive = {
+    case action: ExecuteAction if (
+      action.stateType == "action"
+    )=> {
       
       val nextInternalState = action.actionData.text
 
@@ -43,6 +50,7 @@ case class BlackboxStateActor(data: StateData)
       }
       else if (internalStates.contains(nextInternalState)) {
         internalState = nextInternalState
+        blockingHandlerActor ! ActionExecuted(action)
       } else {
         val receiver = action.asInstanceOf[AnswerAbleMessage].sender
         val message = Failure(new IllegalArgumentException(
@@ -53,7 +61,34 @@ case class BlackboxStateActor(data: StateData)
   }
 
   override protected def getAvailableAction: Array[ActionData] = {
-    internalStates.map((s: String) => ActionData(s, true, exitCondLabel))
+    internalStates.filter((s: String) => !s.equals(internalState)).map((s: String) => ActionData(s, true, exitCondLabel))
+  }
+
+
+  override protected def createAvailableAction: AvailableAction = {
+    var actionData = getAvailableAction
+    log.info("_createAvailableAction.actionData#1: " + actionData.mkString(","))
+    if (timeoutTransition.isDefined) {
+      actionData ++= Array(ActionData("timeout", true, timeoutLabel))
+    }
+    log.info("_createAvailableAction.actionData#2: " + actionData.mkString(","))
+    //if (disabled) {
+      // if disabled, disable all action
+      //actionData.foreach(_.executeAble = false)
+    //}
+    log.info("_createAvailableAction.actionData#3: " + actionData.mkString(","))
+    val aa = AvailableAction(
+      actionIDs(internalState),
+      userID,
+      processInstanceID,
+      subjectID,
+      macroID,
+      id,
+      stateText,
+      "action", //stateType.toString(),
+      actionData)
+    log.info("_createAvailableAction.aa: " + aa)
+    aa
   }
 
 }
