e1fdd1f952cf83b1b64439a8eaa70a75d786e5f8
Fabian Muscariello <fabian.muscariello@mailbox.org>
2014-12-14 15:27:27 +0100

    Adapt boot.scala to more idiomatic scala code

--- ServiceHostUpdate/src/main/scala/de/tkip/serviceupdate/boot.scala
+++ ServiceHostUpdate/src/main/scala/de/tkip/serviceupdate/boot.scala
@@ -7,43 +7,37 @@ import de.tkip.serviceupdate.Messages.UploadServiceToHost
 import java.io.File
 
 object boot extends App {
+  def parseHostPort(arg: String): Option[(String, String)] = {
+    arg.split(":") match {
+      case Array(h, p) => Some(h.replaceAll("localhost", "127.0.0.1"), p)
+      case _           => None
+    }
+  }
+
   println("started")
   if (args.length > 0) {
-    
-    var host: String = null
-    var port: String = null
-    var serviceId: String = null
-    var classPath: String = null
-    var className: String = null
-    var jsonName: String = null
-    
     /** starting actor system */
     val system = ActorSystem("serviceupdater")
-  
+
     implicit val updateActor = system.actorOf(Props[ServiceUpdateActor], "serviceUpdateActor")
-  
+
     if (args.length == 1) {
-    	if(args(0).contains(":")) {
-    		host = args(0).split(":")(0).replaceAll("localhost", "127.0.0.1")
-    		port = args(0).split(":")(1)
-    		updateActor ! UpdateRepository(host, port)
-    	} else {
-    	  println("target in wrong format. Expected: \"hostname:port\", given: " + args(0))
-    	} 
+      parseHostPort(args(0)) match {
+        case Some((host, port)) => updateActor ! UpdateRepository(host, port)
+        case None               => println("target in wrong format. Expected: \"hostname:port\", given: " + args(0))
+      }
     } else if (args.length == 4) {
-    
-      host = args(0).split(":")(0).replaceAll("localhost", "127.0.0.1")
-      port = args(0).split(":")(1)
-      serviceId = args(1)
-      className = args(2)
-      if (!className.endsWith(".class")) className = className + ".class" 
-      jsonName = args(3)      
-      
-      updateActor ! UploadServiceToHost(host, port, serviceId, className, jsonName)
+      val serviceId = args(1)
+      val className = if (!args(2).endsWith(".class")) args(2) + ".class" else args(2)
+      val jsonName = args(3)
+      parseHostPort(args(0)) match {
+        case Some((host, port)) => updateActor ! UploadServiceToHost(host, port, serviceId, className, jsonName)
+        case None               => println("target in wrong format. Expected: \"hostname:port\", given: " + args(0))
+      }
     } else {
       println("Wrong argument count. Expected: 4, Given: " + args.length)
       println("Usage: hostname:port [serviceID serviceClass serviceJSON]")
-	  println("If [serviceID serviceClass serviceJSON] are defined the specified service will be uploaded to the targethost."
+      println("If [serviceID serviceClass serviceJSON] are defined the specified service will be uploaded to the targethost."
 	      + " Otherwise only an UpdateRepository-Message is sent to the host")
     }
   } else {
